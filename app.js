8714f8b00f3b9bb9f7addc48213fe593c28762f5
maruthip <maruthi.putha@doubleverify.com>
2021-07-20 12:36:13 -0400

    Initializing the node app

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.get("/", (req, res, next) => {
+    let timeout = (req.query.timeout) ? parseInt(req.query.timeout) : 0;
+    setTimeout((() => {
+        res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+    }), timeout * 1000);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
