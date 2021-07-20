1e0ddb5197831b328f04637196f81359d98f1d24
maruthip <maruthi.putha@doubleverify.com>
2021-07-20 13:49:03 -0400

    Adding dynamic port assignment

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get("/", (req, res, next) => {
     }), timeout * 1000);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running on port 3000");
 });
